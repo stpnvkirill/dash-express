9aa295e803435c5e98bbe3bf316b34338983a5ff
stpnvkirill <stpnv.kirill.o@gmail.com>
2023-07-28 18:46:39 +0700

    📀 Configuring the Installer

--- setup.py
+++ setup.py
@@ -1,28 +1,32 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
 
-def readme():
-  with open('README.md', 'r') as f:
-    return f.read()
+import setuptools
 
-setup(
-  name='package_name',
-  version='1.0.0',
-  author='your_nickname',
-  author_email='example@gmail.com',
-  description='This is my first module',
-  long_description=readme(),
-  long_description_content_type='text/markdown',
-  url='home_link',
-  packages=find_packages(),
-  install_requires=['requests>=2.25.1'],
-  classifiers=[
-    'Programming Language :: Python :: 3.11',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent'
-  ],
-  keywords='example python',
-  project_urls={
-    'Documentation': 'link'
-  },
-  python_requires='>=3.7'
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dash_express",
+    version="1.0.0",                        # Обновляйте это для каждой новой версии
+    author="Kirill Stepanov",
+    author_email="stpnv.kirill.o@gmail.com",
+    description="A tool for faster application development Plotly Dash",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[                      
+        "dash>=2.11.1",
+        "pandas>=2.0.3",
+        "orjson>=3.9.2",
+        "dash_iconify>=0.1.2",
+        "dash_leaflet>=0.1.28",
+        "flask_caching>=2.0.2",
+        "dash_mantine_components>=0.12.1"                                        
+    ],                                             
+    url="https://github.com/stpnvkirill/dash-express",
+    packages=setuptools.find_packages(),
+    classifiers=(                                 # Классификаторы помогают людям находить 
+        "Programming Language :: Python :: 3",    # ваши проекты. См. все возможные классификаторы 
+        "License :: OSI Approved :: MIT License", # на https://pypi.org/classifiers/
+        "Operating System :: OS Independent",   
+    ),
 )
